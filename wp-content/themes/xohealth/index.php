7151be9f337239b2226b29fd03d84260654bba8f
geruk <geruk@geruk.me>
2011-11-16 16:50:59 -0800

    Move the slide to index only

--- wp-content/themes/xohealth/index.php
+++ wp-content/themes/xohealth/index.php
@@ -5,7 +5,19 @@
  */
 
 get_header(); ?>
-
+	<div id="wrapper">
+	<div class="slider-wrapper theme-default">
+            <div id="slider" class="nivoSlider">
+                <img src="<?php bloginfo('template_directory') ?>/images/slide-1.jpg" alt="" title="The Center for Enhanced Podiatric Function and Pain Management"/>
+                <img src="<?php bloginfo('template_directory') ?>/images/slide-2.jpg" alt="" title="My goal is to determine & treat the CAUSE of your pain" /></a>
+                <img src="<?php bloginfo('template_directory') ?>/images/slide-3.jpg" alt="" />
+                <img src="<?php bloginfo('template_directory') ?>/images/slide-4.jpg" alt="" title="#htmlcaption" />
+            </div>
+            <div id="htmlcaption" class="nivo-html-caption">
+                <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>.
+            </div>
+        </div>
+	</div>
 		<div id="primary">
 			<div id="content" role="main">
 

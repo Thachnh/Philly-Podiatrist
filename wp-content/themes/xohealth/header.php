8955037d1e8db55fd9e5f096a5cdccc09b8ac848
geruk <geruk@geruk-AMZNSEA.(none)>
2011-11-30 00:12:05 -0800

    Changed font to Helvetica Added map 1 4

--- wp-content/themes/xohealth/header.php
+++ wp-content/themes/xohealth/header.php
@@ -37,7 +37,6 @@
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/editor-style.css" />
 <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<link href="http://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css">
 
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/html5.js" type="text/javascript"></script>

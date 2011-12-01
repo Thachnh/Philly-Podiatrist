a3d078dea838ac8912e2190f2dae750848efe0d3
geruk <geruk@geruk-AMZNSEA.(none)>
2011-12-01 05:40:27 -0800

    Tested accordion menu now

--- wp-content/themes/xohealth/index.php
+++ wp-content/themes/xohealth/index.php
@@ -24,8 +24,8 @@ get_header(); ?>
 				jQuery("li.menu-item-type-post_type>a").click(function(event) {
 					jQuery("#content-wrapper").load(jQuery(this).attr("href")+" #content");
 					if (jQuery(this).is("ul#menu-sidebar>li.menu-item>a")) {
-						jQuery("ul#menu-sidebar ul.sub-menu").hide();
-						$(this).siblings("ul.submenu").show();
+						jQuery("ul#menu-sidebar ul.sub-menu:visible").slideUp();
+						jQuery(this).siblings("ul.sub-menu").slideDown();
 					}
 					event.preventDefault();
 				});

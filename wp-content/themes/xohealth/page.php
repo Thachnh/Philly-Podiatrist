5c7a602ec9018881af49778f3d3595d3cfe225cc
Thachnh <thachnhus@gmail.com>
2011-12-02 17:56:41 -0500

    some things

--- wp-content/themes/xohealth/page.php
+++ wp-content/themes/xohealth/page.php
@@ -13,16 +13,11 @@
 		<div id="primary">
 			<?php get_sidebar(); ?>
                         <script type="text/javascript">
-                                jQuery("li.menu-item-type-post_type>a").click(fu
-nction(event) {
-                                        jQuery("#content-wrapper").load(jQuery(t
-his).attr("href")+" #content");
-                                        if (jQuery(this).is("ul#menu-sidebar>li.
-menu-item>a")) {
-                                                jQuery("ul#menu-sidebar ul.sub-m
-enu:visible").slideUp();
-                                                jQuery(this).siblings("ul.sub-me
-nu").slideDown();
+                                jQuery("li.menu-item-type-post_type>a").click(function(event) {
+                                        jQuery("#content-wrapper").load(jQuery(this).attr("href")+" #content");
+                                        if (jQuery(this).is("ul#menu-sidebar>li.menu-item>a")) {
+                                                jQuery("ul#menu-sidebar ul.sub-menu:visible").slideUp();
+                                                jQuery(this).siblings("ul.sub-menu").slideDown();
                                         }
                                         event.preventDefault();
                                 });

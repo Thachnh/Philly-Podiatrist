88e25174b1c7b9962e5eeb8281c2bad72477c588
geruk <geruk@geruk-AMZNSEA.(none)>
2011-12-08 01:58:48 -0800

    Final commit. Fixed shadowbox for ajax loaded images. Added loading gif. Change black font to white in front page. Changed some styles.

--- wp-content/themes/xohealth/menus.js
+++ wp-content/themes/xohealth/menus.js
@@ -1,7 +1,11 @@
 jQuery(document).ready( function() {
 	
 	jQuery("li.menu-item-type-post_type>a").click(function(event) {
-		jQuery("#content-wrapper").load(jQuery(this).attr("href")+" #content");
+		jQuery("h1.entry-title").css({'padding':'4px','background-color':'white','text-align':'center'})
+			.html('<img src="http://phillypodiatrist.phpfogapp.com/wp-content/themes/xohealth/images/loading.gif"/>');
+		jQuery("#content-wrapper").load(jQuery(this).attr("href")+" #content", function() {
+            Shadowbox.init({ skipSetup: true }); Shadowbox.setup(); 
+		});
 		if (jQuery(this).is("ul#menu-sidebar>li.menu-item>a")) {
 			jQuery("ul#menu-sidebar ul.sub-menu:visible").slideUp();
 			jQuery(this).siblings("ul.sub-menu").slideDown();
